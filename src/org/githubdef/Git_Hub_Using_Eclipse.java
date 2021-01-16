69aaf25d66183cbbe45170980bbc899fd111b86f
Ram183 <ramcrazy1992@gmail.com>
2021-01-16 22:46:29 +0530

    New project going to add in Master using Eclipse

---
+++ src/org/githubdef/Git_Hub_Using_Eclipse.java
@@ -0,0 +1,19 @@
+package org.githubdef;
+
+public class Git_Hub_Using_Eclipse {
+
+	public static void main(String[] args) {
+
+		String str = "Ram";
+		
+		if (str == "Ram"|| str=="R" || str == "a") {
+			
+			System.out.println("Employe Name "+ str + " is valid");
+			
+		}else {
+			System.out.println("Is not the valid Employee");
+		}
+
+	}
+
+}
