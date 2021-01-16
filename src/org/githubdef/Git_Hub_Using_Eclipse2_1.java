ac5802dd8a7e93f5800d3325c55d7cb7a4272b69
Ram183 <ramcrazy1992@gmail.com>
2021-01-16 22:54:44 +0530

    Modification done to New Branch

---
+++ src/org/githubdef/Git_Hub_Using_Eclipse2_1.java
@@ -0,0 +1,19 @@
+package org.githubdef;
+
+public class Git_Hub_Using_Eclipse2_1 {
+
+	public static void main(String[] args) {
+
+		String str = "Vishal";
+		
+		if (str == "Ram"|| str=="Vishal" || str == "vishal") {
+			
+			System.out.println("Employe Name "+ str + " is valid");
+			
+		}else {
+			System.out.println("Is not the valid Employee");
+		}
+
+	}
+
+}
